baea1ff092b20c3006d50c95e00936aa6abcd8b4
swanand19 <swanand19ingale@gmail.com>
2025-08-01 23:34:59 +0530

    Third  commit

---
+++ src/Admin/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+
+import styles from '../styles/Dashboard.module.css';
+
+export default function Dashboard() {
+  return (
+    
+    <div className={styles.dashboard}>
+
+      <h1>Welcome to Hospital Management System</h1>
+      <p>Monitor and manage hospital operations here.</p>
+
+    </div>
+  );
+}
---
+++ src/Admin/Medical.jsx
@@ -0,0 +1,11 @@
+// src/pages/Medical.jsx
+import styles from '../styles/Medical.module.css';
+
+export default function Medical() {
+  return (
+    <div className={styles.medical}>
+      <h2>Medical Section</h2>
+      <p>Details about medicines, inventory, or prescriptions can go here.</p>
+    </div>
+  );
+}
